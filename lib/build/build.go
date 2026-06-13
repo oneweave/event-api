0cd1cbe7d1eb1644576040c2ef31d7d14b27258a
Stefan Pfaffel <s.pfaffel@gmail.com>
2026-06-13 21:24:10 +0200

    feat: add build info to build events
    Signed-off-by: Stefan Pfaffel <s.pfaffel@gmail.com>

--- lib/build/build.go
+++ lib/build/build.go
@@ -6,9 +6,15 @@ const (
 	ArtifactBuildFailedV1Type    = "artifact.build.failed.v1"
 )
 
+type BuildSourceInfo struct {
+	Revision  string `json:"revision" bson:"revision" validate:"required"`
+	Commitish string `json:"commitish" bson:"commitish" validate:"required"`
+}
+
 type ArtifactBuildBaseData struct {
-	BuildID   string `json:"buildId" bson:"build_id" validate:"required,uuid"`
-	ReleaseID string `json:"releaseId" bson:"release_id" validate:"required,uuid"`
+	BuildID    string          `json:"buildId" bson:"build_id" validate:"required,uuid"`
+	ReleaseID  string          `json:"releaseId" bson:"release_id" validate:"required,uuid"`
+	SourceInfo BuildSourceInfo `json:"sourceInfo" bson:"source_info" validate:"required,dive"`
 }
 
 func NewArtifactBuildBaseData() ArtifactBuildBaseData {

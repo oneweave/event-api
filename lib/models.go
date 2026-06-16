218c8e8fedcfe4ab062a42a1d403847ac6976e12
Stefan Pfaffel <s.pfaffel@gmail.com>
2026-06-16 20:49:39 +0200

    chore: mark ref as not required
    Signed-off-by: Stefan Pfaffel <s.pfaffel@gmail.com>

--- lib/models.go
+++ lib/models.go
@@ -94,7 +94,7 @@ func NewReleaseSourceImagePullTargetFromPushTarget(pushTarget ReleaseImagePushTa
 // ReleaseSource matches the releaseSource payload shape from artifact.release.requested.v1.
 type ReleaseSource struct {
 	Repository        ReleaseSourceRepository `json:"repository" bson:"repository" validate:"required"`
-	SourceRevisionRef string                  `json:"sourceRevisionRef" bson:"source_revision_ref" validate:"required"`
+	SourceRevisionRef string                  `json:"sourceRevisionRef" bson:"source_revision_ref"`
 	SourceRevision    string                  `json:"sourceRevision" bson:"source_revision" validate:"required"`
 	ManifestFilePath  *string                 `json:"manifestFilePath,omitempty" bson:"manifest_file_path,omitempty" validate:"omitempty"`
 	DockerContextPath *string                 `json:"dockerContextPath,omitempty" bson:"docker_context_path,omitempty" validate:"omitempty"`
